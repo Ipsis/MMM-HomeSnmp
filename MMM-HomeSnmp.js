c55ce71585f2bc9dae558cabc147b3283171c7bd
Ipsis <ipsis418@gmail.com>
2019-10-12 15:44:27 +0100

    Messy table and gauge - but it works

--- MMM-HomeSnmp.js
+++ MMM-HomeSnmp.js
@@ -7,7 +7,6 @@ Module.register("MMM-HomeSnmp", {
     start: function() {
         Log.info("Starting module: " + this);
 
-        var self = this;
         setInterval(() => {
             this.sendSocketNotification("SNMP_REFRESH", { })
         }, 5000);
@@ -23,71 +22,47 @@ Module.register("MMM-HomeSnmp", {
         var usgTxGage = document.createElement('div');
         usgTxGage.id = 'usgTxGage';
 
-        var wapRxGage = document.createElement('div');
-        wapRxGage.id = 'wapRxGage';
-        var wapTxGage = document.createElement('div');
-        wapTxGage.id = 'wapTxGage';
-
-        var p1RxGage = document.createElement('div');
-        p1RxGage.id = 'p1RxGage';
-        var p1TxGage = document.createElement('div');
-        p1TxGage.id = 'p1TxGage';
-        
-        var p2RxGage = document.createElement('div');
-        p2RxGage.id = 'p2RxGage';
-        var p2TxGage = document.createElement('div');
-        p2TxGage.id = 'p2TxGage';
-
-        var p3RxGage = document.createElement('div');
-        p3RxGage.id = 'p3RxGage';
-        var p3TxGage = document.createElement('div');
-        p3TxGage.id = 'p3TxGage';
-
-        var p4RxGage = document.createElement('div');
-        p4RxGage.id = 'p4RxGage';
-        var p4TxGage = document.createElement('div');
-        p4TxGage.id = 'p4TxGage';
-
-        var p5RxGage = document.createElement('div');
-        p5RxGage.id = 'p5RxGage';
-        var p5TxGage = document.createElement('div');
-        p5TxGage.id = 'p5TxGage';
-
-        var p6RxGage = document.createElement('div');
-        p6RxGage.id = 'p6RxGage';
-        var p6TxGage = document.createElement('div');
-        p6TxGage.id = 'p6TxGage';
-
-        var p7RxGage = document.createElement('div');
-        p7RxGage.id = 'p7RxGage';
-        var p7TxGage = document.createElement('div');
-        p7TxGage.id = 'p7TxGage';
-        
-        var p8RxGage = document.createElement('div');
-        p8RxGage.id = 'p8RxGage';
-        var p8TxGage = document.createElement('div');
-        p8TxGage.id = 'p8TxGage';
-
         wrapper.appendChild(usgRxGage);
         wrapper.appendChild(usgTxGage);
-        wrapper.appendChild(wapRxGage);
-        wrapper.appendChild(wapTxGage);
-        wrapper.appendChild(p1RxGage);
-        wrapper.appendChild(p1TxGage);
-        wrapper.appendChild(p2RxGage);
-        wrapper.appendChild(p2TxGage);
-        wrapper.appendChild(p3RxGage);
-        wrapper.appendChild(p3TxGage);
-        wrapper.appendChild(p4RxGage);
-        wrapper.appendChild(p4TxGage);
-        wrapper.appendChild(p5RxGage);
-        wrapper.appendChild(p5TxGage);
-        wrapper.appendChild(p6RxGage);
-        wrapper.appendChild(p6TxGage);
-        wrapper.appendChild(p7RxGage);
-        wrapper.appendChild(p7TxGage);
-        wrapper.appendChild(p8RxGage);
-        wrapper.appendChild(p8TxGage);
+
+        var tableDiv = document.createElement('div');
+        var table = document.createElement('table');
+        tableDiv.appendChild(table);
+        if (this.config.currData.length > 0) {
+            this.config.currData.forEach(element => {
+
+                var row = document.createElement('tr');
+
+                var c1 = document.createElement('td');
+                c1.setAttribute('class', 'title');
+                c1.style.textAlign = 'left';
+                c1.innerText = element.name;
+                row.appendChild(c1);
+
+                var c2 = document.createElement('td');
+                c2.setAttribute('class', 'value');
+                c2.style.textAlign = 'right';
+                if (element.rx == 0) {
+                    c2.innerText = "-";
+                } else {
+                    c2.innerText = this.readableBytes(element.rx);
+                }
+                row.appendChild(c2);
+
+                var c3 = document.createElement('td');
+                c3.setAttribute('class', 'value');
+                c3.style.textAlign = 'right';
+                if (element.tx == 0) {
+                    c3.innerText = "-";
+                } else {
+                    c3.innerText = this.readableBytes(element.tx);
+                }
+                row.appendChild(c3);
+
+                table.appendChild(row);
+            })
+        }
+        wrapper.appendChild(tableDiv); 
 
         return wrapper;
     },
@@ -95,39 +70,13 @@ Module.register("MMM-HomeSnmp", {
         if (notification === "SNMP_DATA") {
             this.config.currData = payload;
 
-            payload.forEach(element => {
+            this.config.currData.forEach(element => {
                 if (element.name === "WAN") {
                     usgRx.refresh(element.rx, element.speed, 0, element.name);
                     usgTx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "UWAP") {
-                    wapRx.refresh(element.rx, element.speed, 0, element.name);
-                    wapTx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "Muppets") {
-                    p1Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p1Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "Dev") {
-                    p2Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p2Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "NAS") {
-                    p3Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p3Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "WAP") {
-                    p4Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p4Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "Game") {
-                    p5Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p5Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "Blackness") {
-                    p6Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p6Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "EyeOfEnder") {
-                    p7Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p7Tx.refresh(element.tx, element.speed, 0, element.name);
-                } else if (element.name === "Uplink") {
-                    p8Rx.refresh(element.rx, element.speed, 0, element.name);
-                    p8Tx.refresh(element.tx, element.speed, 0, element.name);
                 }
             });
+            this.updateDom(0);
         }
     }, 
     getScripts: function() {
@@ -137,7 +86,7 @@ Module.register("MMM-HomeSnmp", {
         ]
     },
     getStyles: function() {
-        return [this.file("css/style.css")];
+        return [this.file("css/style.css"), "font-awesome.css"];
     },
     getHeader: function() {
         return "Port Loads";
@@ -151,29 +100,17 @@ Module.register("MMM-HomeSnmp", {
             var script = document.createElement('script');
             script.innerHTML = 
                 'usgRx = new JustGage({id: "usgRxGage", title:"USG RX", ' + fixed + '});' +
-                'usgTx = new JustGage({id: "usgTxGage", title:"USG TX", ' + fixed + '});' +
-                'wapRx = new JustGage({id: "wapRxGage", title:"WAP RX", ' + fixed + '});' +
-                'wapTx = new JustGage({id: "wapTxGage", title:"WAP TX", ' + fixed + '});' +
-                'p1Rx = new JustGage({id: "p1RxGage", title:"P1 RX", ' + fixed + '});' +
-                'p1Tx = new JustGage({id: "p1TxGage", title:"P1 TX", ' + fixed + '});' +
-                'p2Rx = new JustGage({id: "p2RxGage", title:"P2 RX", ' + fixed + '});' +
-                'p2Tx = new JustGage({id: "p2TxGage", title:"P2 TX", ' + fixed + '});' +
-                'p3Rx = new JustGage({id: "p3RxGage", title:"P3 RX", ' + fixed + '});' +
-                'p3Tx = new JustGage({id: "p3TxGage", title:"P3 TX", ' + fixed + '});' +
-                'p4Rx = new JustGage({id: "p4RxGage", title:"P4 RX", ' + fixed + '});' +
-                'p4Tx = new JustGage({id: "p4TxGage", title:"P4 TX", ' + fixed + '});' +
-                'p5Rx = new JustGage({id: "p5RxGage", title:"P5 RX", ' + fixed + '});' +
-                'p5Tx = new JustGage({id: "p5TxGage", title:"P5 TX", ' + fixed + '});' +
-                'p6Rx = new JustGage({id: "p6RxGage", title:"P6 RX", ' + fixed + '});' +
-                'p6Tx = new JustGage({id: "p6TxGage", title:"P6 TX", ' + fixed + '});' +
-                'p7Rx = new JustGage({id: "p7RxGage", title:"P7 RX", ' + fixed + '});' +
-                'p7Tx = new JustGage({id: "p7TxGage", title:"P7 TX", ' + fixed + '});' +
-                'p8Rx = new JustGage({id: "p8RxGage", title:"P8 RX", ' + fixed + '});' +
-                'p8Tx = new JustGage({id: "p8TxGage", title:"P8 TX", ' + fixed + '});'
+                'usgTx = new JustGage({id: "usgTxGage", title:"USG TX", ' + fixed + '});'
 
             document.body.appendChild(script);
         }
     },
+    readableBytes(bytes) {
+        var i = Math.floor(Math.log(bytes) / Math.log(1024)),
+        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    
+        return (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + sizes[i];
+    }
 });
 
 
